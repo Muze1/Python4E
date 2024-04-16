40185114a6d0f7819cb78de42be5d16818c88dce
Umair <67766295+Muze1@users.noreply.github.com>
2024-04-16 15:11:09 +0100

    add to basic

--- basic.py
+++ basic.py
@@ -3,4 +3,14 @@ msg = "Hello World"
 print(msg)
 
 # BMI Calc
+weight = 70
+height = 175
+bmi = weight / (height / 100)
+print(bmi)
 
+# Roll a dice - twice!
+import random
+roll = random.randint(1, 6)
+print(roll)
+roll = random.randint(1, 6)
+print(roll)
