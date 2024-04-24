3dee33b52c984b7eccf17f1784f6a3ef238a634b
Umair <67766295+Muze1@users.noreply.github.com>
2024-04-24 17:31:04 +0100

    Update user_input.py

--- user_input.py
+++ user_input.py
@@ -1,2 +1,24 @@
+import sys
+import random
+
+# Creating Rock, Paper, Scissors game with user input
 print("")
+
 player_choice = input('Enter...\n1 for Rock,\n2 for Paper, or\n3 for Scissors\n\n')
+player = int(player_choice)
+
+if player < 1 or player > 3:
+    sys.exit("You must enter 1, 2 or 3.")
+
+comp_choice = random.choice("123")
+comp = int(comp_choice)
+
+print("")
+print("You chose " + player_choice + ".")
+print("The computer chose " + comp_choice + ".")
+print("")
+
+if player == 1:
+    print("You win!")
+else:
+    print("Computer wins!")
