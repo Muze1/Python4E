81a123b91a73a4ce9b54fbb5c1be95e5c0ac4b14
Umair <67766295+Muze1@users.noreply.github.com>
2024-05-08 17:14:15 +0100

    Update 2_todo_list.py

--- mini_projects/2_todo_list.py
+++ mini_projects/2_todo_list.py
@@ -1,1 +1,3 @@
-# 
+# Simple command-line to-do list where users can add tasks, remove completed tasks, and view their list of tasks
+# Concepts applied: Lists, loops, functions, basic file handling
+
